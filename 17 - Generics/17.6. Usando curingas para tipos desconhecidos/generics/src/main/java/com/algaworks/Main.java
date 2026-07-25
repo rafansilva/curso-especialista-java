c175a01afa08ece10bb423fbe566bfbc3e7135cc
Rafael N. Silva <31461569+rafansilva@users.noreply.github.com>
2026-07-25 16:53:33 -0300

    17.6. Usando curingas para tipos desconhecidos

---
+++ 17 - Generics/17.6. Usando curingas para tipos desconhecidos/generics/src/main/java/com/algaworks/Main.java
@@ -0,0 +1,40 @@
+package com.algaworks;
+
+import com.algaworks.estruturadedados.Colecao;
+import com.algaworks.estruturadedados.ColecaoVaziaException;
+import com.algaworks.estruturadedados.Fila;
+import com.algaworks.estruturadedados.Pilha;
+import com.algaworks.loja.Produto;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Colecao<Produto> produtos = new Fila<>();
+        produtos.colocar(new Produto("Arroz"));
+        produtos.colocar(new Produto("Feijão"));
+        produtos.colocar(new Produto("Água de coco"));
+
+        retirarTodos(produtos);
+
+        Colecao<String> nomes = new Pilha<>();
+        nomes.colocar("João");
+        nomes.colocar("Carlos");
+        nomes.colocar("Maria");
+
+        retirarTodos(nomes);
+    }
+
+    private static void retirarTodos(Colecao<?> objetos) {
+        try {
+            int i = 1;
+            while (true) {
+                Object objeto = objetos.retirar();
+                System.out.printf("%d. %s%n", i, objeto);
+                i++;
+            }
+        } catch (ColecaoVaziaException e) {
+            System.out.println("Coleção de objetos esgotada");
+        }
+    }
+
+}
---
+++ 17 - Generics/17.6. Usando curingas para tipos desconhecidos/generics/src/main/java/com/algaworks/estruturadedados/Pilha.java
@@ -0,0 +1,31 @@
+package com.algaworks.estruturadedados;
+
+import com.algaworks.loja.Produto;
+
+import java.util.Arrays;
+
+public class Pilha<T> implements Colecao<T> {
+
+    private T[] itens;
+
+    public Pilha() {
+        itens = (T[]) new Object[0];
+    }
+
+    public void colocar(T item) {
+        itens = Arrays.copyOf(itens, itens.length + 1);
+        itens[itens.length - 1] = item;
+    }
+
+    public T retirar() {
+        if (itens.length == 0) {
+            throw new ColecaoVaziaException("Pilha sem itens");
+        }
+
+        T item = itens[itens.length - 1];
+        itens = Arrays.copyOf(itens, itens.length - 1);
+
+        return item;
+    }
+
+}
