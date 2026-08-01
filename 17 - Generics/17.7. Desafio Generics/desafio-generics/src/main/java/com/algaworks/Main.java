df78c858d1888345923c236bc5d0083e075b77a0
Rafael N. Silva <31461569+rafansilva@users.noreply.github.com>
2026-08-01 16:23:13 -0300

    17.7. Desafio: Generics

---
+++ 17 - Generics/17.7. Desafio Generics/desafio-generics/src/main/java/com/algaworks/Main.java
@@ -0,0 +1,26 @@
+package com.algaworks;
+
+import com.algaworks.estoque.Carro;
+import com.algaworks.estoque.GerenciadorEstoque;
+import com.algaworks.estoque.Roupa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GerenciadorEstoque<Roupa> estoqueRoupas = new GerenciadorEstoque<>();
+//        estoqueRoupas.adicionar(new Carro("BMW X1", 4));
+        estoqueRoupas.adicionar(new Roupa("Camiseta básica branca", 20));
+        estoqueRoupas.adicionar(new Roupa("Camiseta básica preta", 10));
+        imprimirEstoque(estoqueRoupas);
+
+        GerenciadorEstoque<Carro> estoqueCarros = new GerenciadorEstoque<>();
+        estoqueCarros.adicionar(new Carro("BMW X1", 4));
+        estoqueCarros.adicionar(new Carro("Honda HR-V", 8));
+
+        imprimirEstoque(estoqueCarros);
+    }
+
+    public static void imprimirEstoque(GerenciadorEstoque<?> gerenciadorEstoque) {
+        System.out.println("Estoque: " + gerenciadorEstoque.getQuantidadeTotal());
+    }
+}
