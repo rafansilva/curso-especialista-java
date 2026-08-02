2486786bc8219457310d466ba40ee0f20e6fc4ab
Rafael N. Silva <31461569+rafansilva@users.noreply.github.com>
2026-08-02 00:19:36 -0300

    18.4. Usando listas do tipo ArrayList

---
+++ 18 - Collections Framework/18.4. Usando listas do tipo ArrayList/hotelaria/src/main/java/com/algaworks/Main.java
@@ -0,0 +1,24 @@
+package com.algaworks;
+
+import com.algaworks.agencia.CadastroHotel;
+import com.algaworks.agencia.Hotel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CadastroHotel cadastro = new CadastroHotel();
+        cadastro.adicionar("Jaguaribe Lodge", "Fortim/CE", 1300);
+        cadastro.adicionar("Vila Selvagem", "Fortim/CE", 1400);
+        cadastro.adicionar("Hotel Fazenda Dona Carolina", "Itatiba/SP", 2200);
+        cadastro.adicionar("Tivoli Ecoresort", "Praia do Forte/BA", 2000);
+        cadastro.adicionar("Mercure", "Uberlândia/MG", 400);
+
+//        System.out.println(cadastro.obterTodos());
+
+        Hotel hotel = (Hotel) cadastro.obterTodos().get(1);
+
+        System.out.println(hotel.getNome());
+    }
+
+
+}
