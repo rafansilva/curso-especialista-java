c51bb94225df7506ab134e1fbd9ebcabe66fcfa3
Rafael N. Silva <31461569+rafansilva@users.noreply.github.com>
2026-08-15 23:09:44 -0300

    18.7. Localizando objetos em listas

---
+++ 18 - Collections Framework/18.8. Manipulando objetos da lista/hotelaria/src/main/java/com/algaworks/agencia/CadastroHotel.java
@@ -0,0 +1,22 @@
+package com.algaworks.agencia;
+
+import java.util.ArrayList;
+
+public class CadastroHotel {
+
+    private final ArrayList<Hotel> hoteis = new ArrayList<>();
+
+    public void adicionar(String nome, String cidade, double precoDiaria) {
+        Hotel hotel = new Hotel(nome, cidade, precoDiaria);
+
+//        if (hoteis.contains(hotel)) {
+//            throw new HotelJaExestenteException("Hotel já foi adicionado");
+//        }
+
+        hoteis.add(hotel);
+    }
+
+    public ArrayList<Hotel> obterTodos() {
+        return hoteis;
+    }
+}
