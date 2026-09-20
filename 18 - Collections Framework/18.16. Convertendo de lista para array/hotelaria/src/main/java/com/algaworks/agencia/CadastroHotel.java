ad337d9bcda24c26e158b334eb5957795e8c0121
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2026-09-19 22:12:39 -0300

    18.16. Convertendo de lista para array

---
+++ 18 - Collections Framework/18.16. Convertendo de lista para array/hotelaria/src/main/java/com/algaworks/agencia/CadastroHotel.java
@@ -0,0 +1,48 @@
+package com.algaworks.agencia;
+
+import java.util.*;
+
+public class CadastroHotel {
+
+    private final List<Hotel> hoteis = new ArrayList<>();
+
+    public void adicionar(String nome, String cidade, double precoDiaria) {
+        Hotel hotel = new Hotel(nome, cidade, precoDiaria);
+
+        if (hoteis.contains(hotel)) {
+            throw new HotelJaExestenteException("Hotel já foi adicionado");
+        }
+
+        hoteis.add(hotel);
+    }
+
+    public List<Hotel> obterTodos() {
+        return hoteis;
+    }
+
+    public Hotel[] obterTodosComoArray() {
+        return hoteis.toArray(new Hotel[0]);
+    }
+    
+    public void removerPorCidade(String cidade) {
+        Iterator<Hotel> hotelIterator = hoteis.iterator();
+        while (hotelIterator.hasNext()) {
+            Hotel hotel = hotelIterator.next();
+            if (hotel.getCidade().equals(cidade)) {
+                hotelIterator.remove();
+            }
+        }
+    }
+
+    public void remover(Hotel hotel) {
+        boolean removido = hoteis.remove(hotel);
+
+        if (!removido) {
+            throw new HotelNaoEncontradoException("Hotel não encontrado para remoção");
+        }
+    }
+
+    public void removerTodos() {
+        hoteis.clear();
+    }
+}
